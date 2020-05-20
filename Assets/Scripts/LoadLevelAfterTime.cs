e5238089d34d263e1be10f54e44b3bb00f11e5ea
TomTwinkerbell <36972477+TomTwinkerbell@users.noreply.github.com>
2020-05-20 18:50:24 +0100

    Scene Implement plus Tunes
    Bangers

---
+++ Assets/Scripts/LoadLevelAfterTime.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelAfterTime : MonoBehaviour
+{
+    [SerializeField]
+    private float delayBeforeLoading = 10f;
+    [SerializeField]
+    private string sceneNameToLoad;
+
+    private float timeElaspsed;
+
+    private void Update()
+    {
+        timeElaspsed += Time.deltaTime;
+        if (timeElaspsed > delayBeforeLoading)
+        {
+
+            SceneManager.LoadScene(sceneNameToLoad);
+
+        }
+    }
+}
